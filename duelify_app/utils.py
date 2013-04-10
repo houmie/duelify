0c187e6e10bdba419478f2083db5c130de33bbfe
Hooman <hooman@venuscloud.com>
2013-04-10 14:59:39 +0100

    fixed a bug

--- duelify_app/utils.py
+++ duelify_app/utils.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
 def facebook_save(user, social_user, details, response, *args, **kwargs):
-    user.date_of_birth = datetime.strptime(response.get('birthday'), '%m/%d/%Y')        
-    user.save()       
+    dob = response.get('birthday')
+    if dob:
+        user.date_of_birth = datetime.strptime(response.get('birthday'), '%m/%d/%Y')        
+        user.save()       
